10d3604bac38b3e0f4b2ddb051a11a5c12c7d4b5
rubinapokhrel061 <pokhrelrubina2061.com>
2024-08-10 07:11:31 +0545

    node corn

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import CategoryRoutes from "./Routes/CategoryRoutes.js";
 dotenv.config();
+import cron from "node-cron";
 
+cron.schedule(" */15 * * * *", () => {
+  console.log("running");
+});
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 5000;
 connectToDatabase();
-// app.get("/", (req, res) => {
-//   res.send(message:"success");
-// });
+app.get("/", (req, res) => {
+  res.send("sucess");
+});
 
 app.use("/auth", AuthRoutes);
 app.use("/category", CategoryRoutes);
